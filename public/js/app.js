0c13c9de3257971d827835fcd3b7b87ed4bb5227
Johan <klausenjohan23@gmail.com>
2021-10-14 19:41:10 +0200

    got stuck on the button

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+
+function ShowTime() {
+    var x = document.getElementsById('date1');
+    if(x.style.display == "none")
+    {
+        x.style.display ='block';
+    }
+    else
+    {
+        x.style.display = 'none';
+    }
+}
